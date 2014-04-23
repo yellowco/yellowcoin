27ae38df0b31497e381bb92506ff94322c0681da
Ubuntu <ubuntu@ip-172-31-12-78.us-west-2.compute.internal>
2014-04-23 22:04:17 +0000

    postgresql config

--- yellowcoin/settings/contrib/db.py
+++ yellowcoin/settings/contrib/db.py
@@ -1,9 +1,24 @@
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# database configurations
+#	cf. http://bit.ly/1hl2DGh
 import os
+
+# postgresql
+#	cf. http://bit.ly/1hpWZ13 on configuration
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(os.path.dirname(__file__), '..', '..', 'db.sqlite3'),
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'development',
+		'USER': 'root',
+		'PASSWORD': 'monkeys123',
+		'HOST': 'development.cyacjwnr6zif.us-west-2.rds.amazonaws.com',
+		'PORT': '5432'
 	}
 }
+
+# sqlite
+# DATABASES = {
+#	'default': {
+#		'ENGINE': 'django.db.backends.sqlite3',
+#		'NAME': os.path.join(os.path.dirname(__file__), '..', '..', 'db.sqlite3'),
+#	}
+# }
