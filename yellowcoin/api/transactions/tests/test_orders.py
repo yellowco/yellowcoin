cbf23609e0adc873d2f34deb24f7fa8a1c63617f
Ubuntu <ubuntu@ip-172-31-12-78.us-west-2.compute.internal>
2014-04-28 20:23:27 +0000

    test syntax errors

--- yellowcoin/api/transactions/tests/test_orders.py
+++ yellowcoin/api/transactions/tests/test_orders.py
@@ -76,13 +76,13 @@ class TestOrders(YellowcoinAPITestCase):
 		self.assertTrue('bid_subtotal' in response.data['detail'], response)
 		self.assertTrue('ask_subtotal' not in response.data['detail'])
 
-	def test_routing_behavior(self)
+	def test_routing_behavior(self):
 		bank = self.create_bank_account()
 		btc = self.create_btc_account()
 		self.verify_bank_account(bank.data['id'])
 		response = self.client.post('/api/orders/btc/usd/', {
-			'bid_subtotal':Decimal('1.000000000001')
-			'ask_subtotal':Decimal('1.000000000001')
+			'bid_subtotal':Decimal('1.000000000001'),
+			'ask_subtotal':Decimal('1.000000000001'),
 			'withdrawal_account':bank.data['id'],
 			'deposit_account':btc.data['id']
 		})
@@ -91,8 +91,8 @@ class TestOrders(YellowcoinAPITestCase):
 		self.assertEqual('ask_subtotal', Decimal('1'), 'BTC not rounding properly')
 
 		response = self.client.post('/api/orders/btc/usd/', {
-			'bid_subtotal':Decimal('1.005')
-			'ask_subtotal':Decimal('1.000000005')
+			'bid_subtotal':Decimal('1.005'),
+			'ask_subtotal':Decimal('1.000000005'),
 			'withdrawal_account':bank.data['id'],
 			'deposit_account':btc.data['id']
 		})
