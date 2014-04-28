707bc9b9a2f71a8dd2b8f857dff330713809c8a3
root <root@kevmo314.uchicago.edu>
2014-04-28 12:32:39 -0500

    clean up settings

--- yellowcoin/settings/development.py
+++ yellowcoin/settings/development.py
@@ -1,1 +1,9 @@
 from yellowcoin.settings.default import *
+import os
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': os.path.join(os.path.dirname(__file__), '..', '..', 'db.sqlite3'),
+	}
+}
