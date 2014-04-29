14fbd1156fef422283f26e69f9a350fd724d4e94
Ubuntu <ubuntu@ip-172-31-12-78.us-west-2.compute.internal>
2014-04-29 01:57:58 +0000

    setup.sh now accepts VIRTUALENV as an arg (for testing)

--- setup.sh
+++ setup.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
+
 # install Apache, mod_wsgi, PostgreSQL bindings, and geoip packages
 sudo aptitude install apache2 libapache2-mod-wsgi python-psycopg2 geoip-database-contrib postgresql-client
 sudo aptitude install libpq-dev python-dev
 
-# run the app in a virtual environment
-pip install virtualenv
+if [ '$1'='VIRTUALENV' ];
+then
+	# run the app in a virtual environment
+	pip install virtualenv
 
-virtualenv yc && sudo ln -s ~/yc /var/www/ && cd yc
+	virtualenv yc && sudo ln -s ~/yc /var/www/yc && cd yc
 
-# checkout all user-side apps
-git clone --recursive https://github.com/kevmo314/yellowcoin.git && cd yellowcoin
+	# checkout all user-side apps
+	git clone --recursive https://github.com/kevmo314/yellowcoin.git
+else
+	git clone --recursive https://github.com/kevmo314/yellowcoin.git
 
-# start virtualenv
-. ../bin/activate
+	# production server -- only site would be YC
+	sudo rm -rf /var/www
+	sudo ln -s ~/yellowcoin /var/www
+fi
+
+cd yellowcoin
+
+if [ '$1'='VIRTUALENV' ];
+then
+	# start virtualenv
+	. ../bin/activate
+fi
 
 # install supplementary apps
 # cf. http://bit.ly/RMBJhx to why ez_setup needs to be installed separately
