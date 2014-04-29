92f74f5af97677f31f5cd1bede742acf5d19b58c
Ubuntu <ubuntu@ip-172-31-3-183.us-west-2.compute.internal>
2014-04-29 06:36:10 +0000

    updated setup.sh to link apache server

--- references/setup.sh
+++ references/setup.sh
@@ -9,12 +9,12 @@ sudo aptitude -y upgrade
 sudo aptitude -y install apache2 libapache2-mod-wsgi python-psycopg2 geoip-database-contrib postgresql-client
 sudo aptitude -y install git python-pip libpq-dev python-dev
 
-if [ "$1" = "VIRTUALENV" ];
+if [ "$1" = "STAGING" ];
 then
 	# run the app in a virtual environment
-	pip install virtualenv
+	pip install STAGING
 
-	virtualenv yc && sudo ln -s ~/yc /var/www/yc && cd yc
+	STAGING yc && sudo ln -s ~/yc /var/www/yc && cd yc
 
 	# checkout all user-side apps
 	git clone --recursive https://github.com/kevmo314/yellowcoin.git
@@ -25,9 +25,9 @@ fi
 
 cd yellowcoin
 
-if [ "$1" = "VIRTUALENV" ];
+if [ "$1" = "STAGING" ];
 then
-	# start virtualenv
+	# start STAGING
 	. ../bin/activate
 
 	# install supplementary apps
@@ -41,3 +41,16 @@ fi
 
 # ensure everything is working correctly
 ./manage.py test --settings=yellowcoin.settings.staging
+
+# setup Apache
+if [ "$1" != "STAGING" ];
+then
+	sudo rm /etc/apache2/sites-enabled/*
+	sudo rm /etc/apache2/sites-available/*
+
+	sudo cp references/yellowcoin.conf /etc/apache2/sites-enabled/
+	sudo ln -s /etc/apache2/sites-enabled/yellowcoin.conf /etc/apache2/sites-available/
+
+	sudo tee -a /etc/apache2/apache2.conf < references/wsgipythonpath.txt
+	sudo /etc/init.d/apache2 restart
+fi
