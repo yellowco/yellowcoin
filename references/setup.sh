5784f758d07965455da590c029619972baf09dc2
Ubuntu <ubuntu@ip-172-31-7-77.us-west-2.compute.internal>
2014-05-09 06:09:53 +0000

    fixed setup.sh for HTTP

--- references/setup.sh
+++ references/setup.sh
@@ -32,10 +32,6 @@ case $MODE in
 		# set the default settings for django to be staging.py -- change manually to production.py to commit to live
 		echo "export DJANGO_SETTINGS_MODULE=$SETTINGS" >> ~/.bashrc
 		source ~/.bashrc
-
-		# get around the fact that 'source' does not propagate in a script
-		#	cf. http://bit.ly/RwJNlM
-		exec bash
 		;;
 esac
 
@@ -117,9 +113,14 @@ case $MODE in
 		sudo cp references/yellowcoin.conf /etc/apache2/sites-enabled/
 		sudo ln -s /etc/apache2/sites-enabled/yellowcoin.conf /etc/apache2/sites-available/
 
-		echo "SetEnv DJANGO_SETTINGS_MODULE $SETTINGS" >> references/wsgi_setup.txt
+		# set the environment variable in the server instance
+		echo "SetEnv DJANGO_SETTINGS_MODULE $SETTINGS" >> references/set_env.txt
 
 		sudo tee -a /etc/apache2/apache2.conf < references/wsgi_setup.txt
+		sudo tee -a /etc/apache2/apache2.conf < references/set_env.txt
+
+		rm references/set_env.txt
+
 		sudo /etc/init.d/apache2 restart
 		;;
 	"ENQ")
