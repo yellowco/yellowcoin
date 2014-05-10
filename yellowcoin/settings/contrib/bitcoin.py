3ce83cb8156a5305ab27a6a95a7ccbb68393ec14
Kevin Wang <kevmo314@gmail.com>
2014-05-10 16:02:03 -0500

    Update bitcoin.py

--- yellowcoin/settings/contrib/bitcoin.py
+++ yellowcoin/settings/contrib/bitcoin.py
@@ -4,7 +4,7 @@
 from bitcoinrpc import connect_to_remote, connect_to_local
 
 # btc_conn = connect_to_remote('13PgfhMMjgBT9m3uxP3UaQLQdHnZgBQK8U', '95o6YnhXkbv1fcmqgUp4Qgs8wGqWHgMncntehz2HU9z6', host='rpc.blockchain.info', port=443, use_https=True)
-BTC_CONN = connect_to_remote('Yellowcoin', 't4L3DRgeAb', host='54.186.24.74')
+BTC_CONN = connect_to_remote('yellowcoin', 'kyqDyBc3w2yaAgrEBTCVFAUPBYGALzLn3fZNQxwPMQWUZyhMvrgU4nT4vGmsVYTk', host='54.186.24.74')
 
 # number of confirmation on the transaction before we accept the transaction as true
 BTC_MINCONF = 4
