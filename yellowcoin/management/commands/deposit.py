441956f17b2f43b899ba25695af0f0b9fef96849
root <root@kevmo314.uchicago.edu>
2014-05-19 19:03:55 -0500

    added ./manage.py deposit

---
+++ yellowcoin/management/commands/deposit.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand, CommandError
+from yellowcoin.currencypool.models import POOLS
+from yellowcoin.transactions.tasks import get_pool
+from optparse import make_option
+
+class Command(BaseCommand):
+	args = 'amount rate'
+	help = 'invalid or incomplete arguments -- try running with -h for list of available options'
+	# list of input options for this command
+	#	cf. http://bit.ly/1h1IVMp, http://bit.ly/1jx8VFa
+	option_list = BaseCommand.option_list + (
+		make_option(
+			'--amount',
+			action='store',
+			dest='amount',
+			help='amount of BTC to deposit'),
+		make_option(
+			'--exchange_rate',
+			action='store',
+			dest='exchange_rate',
+			help='BTC:USD rate')
+	)
+	def handle(self, *args, **options):
+		if((not options.get('amount')) or (not options.get('exchange_rate'))):
+			raise CommandError(self.help)
+		pool = get_pool('USD', 'BTC')
+		pool.add(options.get('amount'), options.get('exchange_rate'))
+
