d388ae0f1cdddd4f7a8b0be829b9dd1dac187e70
root <root@kevmo314.uchicago.edu>
2014-05-22 13:52:26 -0500

    wtf

--- yellowcoin/main/templates/main/controllers/HistoryController.js
+++ yellowcoin/main/templates/main/controllers/HistoryController.js
@@ -7,6 +7,14 @@ function HistoryController($scope, $location, $modal, $http, $routeParams) {
 		'Received':4,
 		'Completed':5
 	};
+	$scope.readable = {
+		'Uninitialized': 'Waiting for Server',
+		'Initialized': 'Preparing Transaction',
+		'Pending': 'Waiting for User Activity',
+		'Queried': 'In Progress',
+		'Received': 'Finalizing Transaction',
+		'Completed': 'Transaction Completed'
+	};
 	($scope.reloadTransactions = function() {
 		$scope.reloading = true;
 		$http.get('/api/transactions/').success(function(data) {
